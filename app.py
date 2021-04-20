5d901d2817f02ee7f12a5e68099e64bdf0315f44
letuone <xtaywang7@126.com>
2021-04-20 17:55:14 +0800

    修改文件

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 from flask import Flask,escape
 app=Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    username=request.args.get('name','Flask')
+    return 'Hello, %s!' % escape(username)
+    
